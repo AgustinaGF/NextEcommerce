24698d982c3363da6f07f117decab23c0766928e
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-05-07 20:56:19 -0300

    cart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,75 @@
 import { GetStaticProps } from "next";
+import { Grid, Stack, Text, Button, Link } from "@chakra-ui/react";
+
 import { Product } from "../products/types";
+import api from "../products/api";
+import { useMemo, useState } from "react";
 
 interface Props {
 	products: Product[];
 }
 
-const IndexRoute: React.FC<Props> = () => {
-	return <div>index</div>;
+function parseCurrency(value: number): string {
+	return value.toLocaleString("es-AR", {
+		style: "currency",
+		currency: "ARS",
+	});
+}
+const IndexRoute: React.FC<Props> = ({ products }) => {
+	const [cart, setCart] = useState<Product[]>([]);
+
+	const text = useMemo(() => {
+		return cart
+			.reduce(
+				(message, product) =>
+					message.concat(
+						`* ${product.title} - ${parseCurrency(product.price)}\n`
+					),
+				``
+			)
+			.concat(
+				`\nTotal: ${parseCurrency(
+					cart.reduce((total, product) => total + product.price, 0)
+				)}`
+			);
+	}, [cart]);
+
+	function handleAddToCart(product: Product) {
+		setCart((cart) => cart.concat(product));
+	}
+
+	return (
+		<Stack>
+			<Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+				{products.map((product) => (
+					<Stack backgroundColor="gray.100" key={product.id}>
+						<Text>{product.title}</Text>
+						<Text>{parseCurrency(product.price)}</Text>
+						<Button onClick={() => handleAddToCart(product)} colorScheme="blue">
+							Add
+						</Button>
+					</Stack>
+				))}
+			</Grid>
+			{Boolean(cart.length) && (
+				<Button
+					as={Link}
+					href={`https://wa.me/549114156839?text=${encodeURIComponent(text)}`}
+					isExternal
+					colorScheme="whatsapp"
+				>
+					Complete Order ({cart.length} products)
+				</Button>
+			)}
+		</Stack>
+	);
 };
 
 export const getStaticProps: GetStaticProps = async () => {
+	const products = await api.list();
 	return {
 		props: {
-			products: [],
+			products,
 		},
 	};
 };
---
+++ products/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Papa from "papaparse";
+import { Product } from "./types";
+
+export default {
+	list: async (): Promise<Product[]> => {
+		return axios
+			.get(
+				`https://docs.google.com/spreadsheets/d/e/2PACX-1vT-zOoG-24hIgf7sAc-5XWSsztMVMBN23mar70bddptwNMQa1UEe5FnirsUyoHMXxl6nyWyAJy1WaCi/pub?output=csv`,
+				{ responseType: "blob" }
+			)
+			.then((response) => {
+				return new Promise<Product[]>((resolve, reject) => {
+					Papa.parse(response.data, {
+						header: true,
+						complete: (results) => {
+							const products = results.data as Product[];
+							return resolve(
+								products.map((product) => ({
+									...product,
+									price: Number(product.price),
+								}))
+							);
+						},
+						error: (error) => reject(error.message),
+					});
+				});
+			});
+	},
+};
