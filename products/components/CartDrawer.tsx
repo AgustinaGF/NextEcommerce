58274d700d898ac3421e3f72abfc8cd5cb73a691
AgustinaGF <agustinagomez385@gmail.com>
2023-07-17 16:59:10 -0300

    add changes

--- products/components/CartDrawer.tsx
+++ products/components/CartDrawer.tsx
@@ -18,7 +18,7 @@ import {
 import { CartItem, Product } from "../types";
 import { INFORMATION } from "../../app/constants";
 import { parseCurrency } from "../../utils/currency";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 interface Props extends Omit<DrawerProps, "children"> {
   items: CartItem[];
@@ -30,6 +30,7 @@ const CartDrawer: React.FC<Props> = ({
   items,
   onDecrement,
   onIncrement,
+  onClose,
   ...props
 }) => {
   const total = useMemo(
@@ -57,65 +58,81 @@ const CartDrawer: React.FC<Props> = ({
     [items, total]
   );
 
+  useEffect(() => {
+    if (!items.length) {
+      onClose();
+    }
+  }, [items.length, onClose]);
+
   return (
     <div>
       {
-        <Drawer placement="right" {...props}>
+        <Drawer placement="right" onClose={onClose} {...props}>
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
             <DrawerHeader>Your Order</DrawerHeader>
             <DrawerBody>
-              <Stack spacing={4} divider={<Divider />}>
-                {items.map((product) => (
-                  <Stack direction="row" key={product.id}>
-                    <Stack width="100%">
-                      <Stack direction="row" justifyContent="space-between">
-                        <Text fontWeight="500">
-                          {product.title}
-                          {product.quantity > 1
-                            ? `( x${product.quantity})`
-                            : ""}
-                        </Text>
-                        <Text color="green.400">
-                          {parseCurrency(product.price * product.quantity)}
-                        </Text>
-                        <HStack spacing={3}></HStack>
-                      </Stack>
-                      <Stack direction="row">
-                        <Button size="xs" onClick={() => onDecrement(product)}>
-                          -
-                        </Button>
-                        <Text>{product.quantity}</Text>
-                        <Button size="xs" onClick={() => onIncrement(product)}>
-                          +
-                        </Button>
+              {Boolean(items.length) ? (
+                <Stack spacing={4} divider={<Divider />}>
+                  {items.map((product) => (
+                    <Stack direction="row" key={product.id}>
+                      <Stack width="100%">
+                        <Stack direction="row" justifyContent="space-between">
+                          <Text fontWeight="500">
+                            {product.title}
+                            {product.quantity > 1
+                              ? `( x${product.quantity})`
+                              : ""}
+                          </Text>
+                          <Text color="green.400">
+                            {parseCurrency(product.price * product.quantity)}
+                          </Text>
+                          <HStack spacing={3}></HStack>
+                        </Stack>
+                        <Stack direction="row">
+                          <Button
+                            size="xs"
+                            onClick={() => onDecrement(product)}>
+                            -
+                          </Button>
+                          <Text>{product.quantity}</Text>
+                          <Button
+                            size="xs"
+                            onClick={() => onIncrement(product)}>
+                            +
+                          </Button>
+                        </Stack>
                       </Stack>
                     </Stack>
-                  </Stack>
-                ))}
-              </Stack>
+                  ))}
+                </Stack>
+              ) : (
+                <Text color="gray.400">There are not items in your cart</Text>
+              )}
             </DrawerBody>
 
-            <DrawerFooter>
-              <Button
-                as={Link}
-                href={`https://wa.me/${
-                  INFORMATION.phone
-                }?text=${encodeURIComponent(text)}`}
-                isExternal
-                colorScheme="whatsapp"
-                width="fit-content"
-                padding={4}
-                leftIcon={
-                  <Image
-                    src="https://icongr.am/fontawesome/whatsapp.svg?size=32&color=ffffff"
-                    alt="icon"
-                  />
-                }>
-                Complete Order ({total})
-              </Button>
-            </DrawerFooter>
+            {Boolean(items.length) && (
+              <DrawerFooter>
+                <Button
+                  as={Link}
+                  href={`https://wa.me/${
+                    INFORMATION.phone
+                  }?text=${encodeURIComponent(text)}`}
+                  isExternal
+                  colorScheme="whatsapp"
+                  width="fit-content"
+                  padding={4}
+                  leftIcon={
+                    <Image
+                      src="https://icongr.am/fontawesome/whatsapp.svg?size=32&color=ffffff"
+                      alt="icon"
+                    />
+                  }>
+                  Complete Order ({total})
+                </Button>
+              </DrawerFooter>
+            )}
           </DrawerContent>
         </Drawer>
       }
